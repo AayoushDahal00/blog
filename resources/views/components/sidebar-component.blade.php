f1e45a74dbfbc5e332b8024d5948b6554c0c9fc3
aayoush dahal <ayu.dahal2018@gmail.com>
2023-03-23 08:41:27 +0545

    commited

---
+++ resources/views/components/sidebar-component.blade.php
@@ -0,0 +1,33 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" /> <span
+            class="logo-name">BLOG</span>
+        </a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+          <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="menu-toggle nav-link has-dropdown"><i
+              data-feather="briefcase"></i><span>category</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
+            <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Post</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="chat.html">Chat</a></li>
+            <li><a class="nav-link" href="portfolio.html">Portfolio</a></li>
+            <li><a class="nav-link" href="blog.html">Blog</a></li>
+            <li><a class="nav-link" href="calendar.html">Calendar</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </aside>
+  </div>
